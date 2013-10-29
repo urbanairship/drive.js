fb4de85886f684b113a5c87863562fcdacf1b3d6
jesse keane <me@jessekeane.me>
2013-10-29 13:42:16 -0700

    Write config to suite rather than using an XHR

--- public/suite.js
+++ public/suite.js
@@ -3,7 +3,6 @@
 (function(exports) {
   var console = window.console || {}
     , test_suite
-    , config
 
   function profile_start() {
     return
@@ -303,22 +302,9 @@
     var ready_interval = setInterval(function() {
       if(document.readyState === 'complete') {
         clearInterval(ready_interval)
-        get_config(start_suite)
+        start_suite()
       }
     }, 100)
-
-    function get_config(ready) {
-      var xhr = new XMLHttpRequest()
-
-      xhr.open('GET', '_config/')
-      xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4) {
-          config = __JSON__.parse(xhr.responseText)
-          ready()
-        }
-      }
-      xhr.send(null)
-    }
 
     function start_suite() {
       if(!config.browserify) {
